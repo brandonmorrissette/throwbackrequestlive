833bdf63124e25391791d4f9ea5ec3247e6b884a
Officer Admin <developeralt@protonmail.com>
2024-11-07 17:48:53 -0500

    Second big update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import csv
 
 app = Flask(__name__)
@@ -10,16 +10,16 @@ def home():
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             events.append({
-                'title': row['event_name'], 
-                'start': row['date'],        
-                'location': row['location'],  
+                'name': row['name'], 
+                'date': row['date'],        
+                'venue': row['venue'],  
                 'address': row['address']     
             })
     return render_template('home.html', events=events)
     
 @app.route("/tip")
 def tip():
-    return render_template("tip.html")
+    return redirect("https://tiptopjar.com/throwbackrequestlive", code=302)
     
 @app.route("/contact")
 def contact():
@@ -28,6 +28,10 @@ def contact():
 @app.route("/survey")
 def survey():
     return render_template("survey.html")
+
+@app.route("/socials")
+def socials():
+    return render_template("socials.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
