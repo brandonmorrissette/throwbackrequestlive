efe7e793afca5cf432744627f68eda7c807e9d36
Officer Admin <developeralt@protonmail.com>
2024-11-11 16:34:54 -0500

    Updated csv structure. Made lots of changes to style based on feedback.

--- app.py
+++ app.py
@@ -1,37 +1,43 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 import csv
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+def get_events():
     events = []
     with open('data/events.csv', mode='r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
-            events.append({
-                'name': row['name'], 
-                'date': row['date'],        
-                'venue': row['venue'],  
-                'address': row['address']     
-            })
+            event_datetime = datetime.strptime(f"{row['date']} {row['time']}", '%m-%d-%Y %I:%M %p')
+            if event_datetime >= datetime.now():
+                events.append({
+                    'name': row['name'],
+                    'date': row['date'],
+                    'time': row['time'],
+                    'venue': row['venue'],
+                    'street': row['street'],
+                    'city': row['city'],
+                    'state': row['state']
+                })
+    return events
+
+@app.route('/')
+def home():
+    events = get_events()
     return render_template('home.html', events=events)
-    
+
 @app.route("/tip")
 def tip():
-    return redirect("https://tiptopjar.com/throwbackrequestlive", code=302)
-    
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-    
+    return render_template("tip.html")
+
+@app.route("/book")
+def book():
+    return render_template("book.html")
+
 @app.route("/survey")
 def survey():
     return render_template("survey.html")
-
-@app.route("/socials")
-def socials():
-    return render_template("socials.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
