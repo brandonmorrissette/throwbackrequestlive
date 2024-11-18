bb998f06798f323f762d203f0598a5cfab192fa8
Officer Admin <developeralt@protonmail.com>
2024-11-17 20:44:32 -0500

    Creating Cognito user pool and admin groups and users.

--- infra/stack.py
+++ infra/stack.py
@@ -10,46 +10,44 @@ from aws_cdk import (
     aws_rds as rds,
     aws_iam as iam,
     aws_s3_assets as s3_assets,
+    aws_cognito as cognito,
     aws_logs,
     Duration,
     CfnOutput,
     Size,
+    Tags,
 )
 
 from constructs import Construct
 from pathlib import Path
 
+
 class ThrowbackRequestLiveStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # VPC for networking
+
         vpc = ec2.Vpc(self, "ThrowbackRequestLiveVPC", max_azs=2)
 
-        # ECS Cluster
         cluster = ecs.Cluster(self, "ThrowbackRequestLiveCluster", vpc=vpc)
 
-        # Route53 Hosted Zone
         hosted_zone = route53.HostedZone.from_lookup(
             self, "HostedZone",
             domain_name="throwbackrequestlive.com"
         )
 
-        # SSL Certificate for the domain
-        certificate = acm.Certificate(
+            certificate = acm.Certificate(
             self, "SiteCertificate",
             domain_name="throwbackrequestlive.com",
             subject_alternative_names=["www.throwbackrequestlive.com"],
             validation=acm.CertificateValidation.from_dns(hosted_zone)
         )
 
-        # Docker image for the application
         docker_image = ecr_assets.DockerImageAsset(
             self, "LocalThrowbackRequestLiveImage",
             directory="."
         )
 
-        # Fargate Service
         fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
             self, "FargateService",
             cluster=cluster,
@@ -79,7 +77,6 @@ class ThrowbackRequestLiveStack(Stack):
             healthy_http_codes="200"
         )
 
-        # Route53 DNS Records
         route53.ARecord(
             self, "AliasRecord",
             zone=hosted_zone,
@@ -99,13 +96,11 @@ class ThrowbackRequestLiveStack(Stack):
             description="Public DNS of the Load Balancer"
         )
 
-        # Security Group for RDS
         rds_security_group = ec2.SecurityGroup(self, "RDSSecurityGroup", vpc=vpc)
         rds_security_group.add_ingress_rule(
             ec2.Peer.ipv4(vpc.vpc_cidr_block), ec2.Port.tcp(5432), "Allow ECS to access RDS"
         )
 
-        # RDS Instance
         db_instance = rds.DatabaseInstance(
             self, "RDSInstance",
             engine=rds.DatabaseInstanceEngine.postgres(
@@ -130,6 +125,47 @@ class ThrowbackRequestLiveStack(Stack):
             description="RDS Endpoint"
         )
 
+        user_pool = cognito.UserPool(
+            self, "UserPool",
+            self_sign_up_enabled=False,
+            user_pool_name="UserPool",
+            sign_in_aliases=cognito.SignInAliases(email=True),
+            password_policy=cognito.PasswordPolicy(
+                min_length=12,
+                require_lowercase=True,
+                require_uppercase=True,
+                require_digits=True,
+                require_symbols=True
+            ),
+            account_recovery=cognito.AccountRecovery.EMAIL_ONLY
+        )
+
+        app_client = user_pool.add_client(
+            "UserPoolAppClient",
+            auth_flows=cognito.AuthFlow(
+                admin_user_password=True,
+                user_password=True
+            )
+        )
+
+        admin_group = cognito.CfnUserPoolGroup(
+            self, "AdminGroup",
+            group_name="Admin",
+            user_pool_id=user_pool.user_pool_id
+        )
+
+        CfnOutput(
+            self, "UserPoolId",
+            value=user_pool.user_pool_id,
+            description="Cognito User Pool ID"
+        )
+
+        CfnOutput(
+            self, "AppClientId",
+            value=app_client.user_pool_client_id,
+            description="Cognito App Client ID"
+        )
+
         ec2_role = iam.Role(
             self, "EC2InstanceRole",
             assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
@@ -147,8 +183,8 @@ class ThrowbackRequestLiveStack(Stack):
             ec2.Peer.any_ipv4(), ec2.Port.all_traffic(), "Allow outbound traffic"
         )
 
-        ec2_instance = ec2.Instance(
-            self, "SchemaDeploymentInstance",
+        deployment_ec2_instance = ec2.Instance(
+            self, "DeploymentInstance",
             instance_type=ec2.InstanceType("t3.micro"),
             machine_image=ec2.MachineImage.latest_amazon_linux(),
             vpc=vpc,
@@ -157,9 +193,11 @@ class ThrowbackRequestLiveStack(Stack):
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
         )
 
-        ebs_volume = ec2.Volume(
+        Tags.of(deployment_ec2_instance).add("Name", "DeploymentInstance")
+
+        deployment_ebs_volume = ec2.Volume(
             self,
-            "SchemaVolume",
+            "DeploymentVolume",
             availability_zone=vpc.availability_zones[0],
             size=Size.gibibytes(1),
             volume_type=ec2.EbsDeviceVolumeType.GP2
@@ -168,8 +206,8 @@ class ThrowbackRequestLiveStack(Stack):
         ec2.CfnVolumeAttachment(
             self,
             "VolumeAttachment",
-            instance_id=ec2_instance.instance_id,
-            volume_id=ebs_volume.volume_id,
+            instance_id=deployment_ec2_instance.instance_id,
+            volume_id=deployment_ebs_volume.volume_id,
             device="/dev/xvdf"
         )
 
@@ -179,9 +217,9 @@ class ThrowbackRequestLiveStack(Stack):
             path=str(Path(__file__).parent.parent / "schema")
         )
 
-        schema_dir_asset.grant_read(ec2_instance.role)
+        schema_dir_asset.grant_read(deployment_ec2_instance.role)
 
-        ec2_instance.add_user_data(
+        deployment_ec2_instance.add_user_data(
             "yum update -y",
             "amazon-linux-extras enable postgresql14",
             "yum install -y postgresql unzip aws-cli",
@@ -192,4 +230,3 @@ class ThrowbackRequestLiveStack(Stack):
             f"aws s3 cp {schema_dir_asset.s3_object_url} /mnt/schema/schema.zip",
             "unzip /mnt/schema/schema.zip -d /mnt/schema/"
         )
-
