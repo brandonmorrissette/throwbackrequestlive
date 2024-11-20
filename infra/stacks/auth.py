26b75eb0f74122134d320fca68f319892b7e9a7c
Officer Admin <developeralt@protonmail.com>
2024-11-20 18:03:51 -0500

    Fixing the parent of auth stack

--- infra/stacks/auth.py
+++ infra/stacks/auth.py
@@ -1,9 +1,9 @@
 
-from aws_cdk import core
+from .stack import Stack
 from constructs import Construct
 from infra.constructs.cognito import CognitoConstruct
 
-class AuthStack(core.Stack):
+class AuthStack(Stack):
     def __init__(self, scope: Construct, id: str, superuser_email: str, rds, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
