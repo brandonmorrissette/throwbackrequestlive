73d4d3ffb8df707bd839ec90838ff3360309f20c
Officer Admin <developeralt@protonmail.com>
2024-12-01 19:15:45 -0500

    Hard coding with the vpc id I see in the console

--- backend/infra/constructs/runtime_ecs.py
+++ backend/infra/constructs/runtime_ecs.py
@@ -72,11 +72,12 @@ class RuntimeEcsConstruct(Construct):
         vpc = ec2.Vpc.from_lookup(
             self,
             "ImportedVPC",
-            vpc_id=ssm.StringParameter.from_string_parameter_name(
-                self,
-                "VpcIdParam",
-                string_parameter_name=f"/{project_name}/vpc-id",
-            ).string_value,
+            # vpc_id=ssm.StringParameter.from_string_parameter_name(
+            #     self,
+            #     "VpcIdParam",
+            #     string_parameter_name=f"/{project_name}/vpc-id",
+            # ).string_value,
+            vpc_id=f"{project_name}-vpc",
         )
 
         cluster = ecs.Cluster.from_cluster_attributes(
