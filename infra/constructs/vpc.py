6be1d3b63a33d007faf7a9529570624a1c806156
Officer Admin <developeralt@protonmail.com>
2025-01-23 19:20:05 -0500

    Testing a theory that CfNoutput must be on a stack

--- infra/constructs/vpc.py
+++ infra/constructs/vpc.py
@@ -1,4 +1,3 @@
-from aws_cdk import CfnOutput
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ssm as ssm
 from constructs import Construct
@@ -14,11 +13,4 @@ class VpcConstruct(Construct):
             "VpcIdParam",
             parameter_name=f"/{project_name}/vpc-id",
             string_value=self.vpc.vpc_id,
-        )
-        CfnOutput(
-            self,
-            "subnet-id",
-            value=self.vpc.select_subnets(
-                subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT
-            ).subnet_ids[0],
         )
