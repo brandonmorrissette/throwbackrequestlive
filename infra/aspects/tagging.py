4a3ab3f660d768e1d5370b367946f5042792a248
Officer Admin <developeralt@protonmail.com>
2025-01-30 19:01:47 -0500

    explicitly naming the tags

--- infra/aspects/tagging.py
+++ infra/aspects/tagging.py
@@ -5,11 +5,12 @@ from constructs import IConstruct
 
 class ConfigTaggingAspect(IAspect):
 
-    def __init__(self, env: Config):
-        self.env = env
+    def __init__(self, config: Config):
+        self.config = config
 
     def visit(self, node: IConstruct):
         if isinstance(node, CfnResource):
-            for key, value in vars(self.env).items():
-                if value:
-                    Tags.of(node).add(key, str(value))
+            Tags.of(node).add("project_name", self.config.project_name)
+            Tags.of(node).add("environment_name", self.config.environment_name)
+            Tags.of(node).add("account", self.config.cdk_environment.account)
+            Tags.of(node).add("region", self.config.cdk_environment.region)
