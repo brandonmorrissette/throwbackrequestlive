a8aaee4e68d06e1e297fb8bbdb97b96ad8c2bf43
Officer Admin <developeralt@protonmail.com>
2025-02-04 11:36:03 -0500

    Copying pattern found online for aspects

--- infra/aspects/tagging.py
+++ infra/aspects/tagging.py
@@ -1,14 +1,16 @@
+import jsii
 from aws_cdk import IAspect, Tags
 from config import Config
 from constructs import IConstruct
 
 
-class ConfigTaggingAspect(IAspect):
+@jsii.implements(IAspect)
+class ConfigTaggingAspect:
 
-    def __init__(self, env: Config):
-        self.env = env
+    def __init__(self, config: Config):
+        self.config = config
 
     def visit(self, node: IConstruct):
         if isinstance(node, IConstruct):
-            Tags.of(node).add("project_name", self.env.project_name)
-            Tags.of(node).add("environment_name", self.env.environment_name)
+            Tags.of(node).add("project_name", self.config.project_name)
+            Tags.of(node).add("environment_name", self.config.environment_name)
