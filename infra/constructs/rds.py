ed66c6e115daa8129c607cfb3a528783cd40d777
Officer Admin <developeralt@protonmail.com>
2025-02-04 18:18:48 -0500

    Better separation of duties with storage stack and rds construct

--- infra/constructs/rds.py
+++ infra/constructs/rds.py
@@ -1,26 +1,36 @@
-from aws_cdk import aws_ec2 as ec2, aws_rds as rds, Duration
+from aws_cdk import Duration, RemovalPolicy
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_logs as logs
+from aws_cdk import aws_rds as rds
 from aws_cdk import aws_ssm as ssm
+from config import Config
 from constructs import Construct
 
 
 class RdsConstruct(Construct):
-    def __init__(self, scope: Construct, id: str, vpc, project_name: str) -> None:
+    def __init__(self, scope: Construct, id: str, vpc, config: Config) -> None:
         super().__init__(scope, id)
 
         rds_security_group = ec2.SecurityGroup(self, "rds-security-group", vpc=vpc)
         rds_security_group.add_ingress_rule(
-            ec2.Peer.ipv4(vpc.vpc_cidr_block), ec2.Port.tcp(5432), "Allow ECS to access RDS"
+            ec2.Peer.ipv4(vpc.vpc_cidr_block),
+            ec2.Port.tcp(5432),
+            "Allow ECS to access RDS",
         )
 
-        db_name = project_name
         ssm.StringParameter(
-            self, "DbNameParam",
-            parameter_name=f"/{project_name}/db-name",
-            string_value=db_name
+            self,
+            "DbNameParam",
+            parameter_name=f"/{config.project_name}/db-name",
+            string_value=config.project_name,
         )
+
         self.db_instance = rds.DatabaseInstance(
-            self, "rds-instance",
-            database_name=db_name,
+            self,
+            "rds-instance",
+            database_name=config.project_name,
             engine=rds.DatabaseInstanceEngine.postgres(
                 version=rds.PostgresEngineVersion.VER_16_4
             ),
@@ -29,13 +39,124 @@ class RdsConstruct(Construct):
             ),
             vpc=vpc,
             credentials=rds.Credentials.from_generated_secret(
-                "db_master_user",
-                secret_name=f"{project_name}-db-credentials"
+                "db_master_user", secret_name=f"{config.project_name}-db-credentials"
             ),
             allocated_storage=20,
             multi_az=False,
             publicly_accessible=False,
             backup_retention=Duration.days(7),
             security_groups=[rds_security_group],
-            instance_identifier=f"{project_name}-rds-instance"
+            instance_identifier=f"{config.project_name}-rds-instance",
         )
+
+        task_role = iam.Role(
+            self,
+            "SQLTaskRole",
+            assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+            inline_policies={
+                "SQLTaskPolicy": iam.PolicyDocument(
+                    statements=[
+                        iam.PolicyStatement(
+                            actions=[
+                                "secretsmanager:GetSecretValue",
+                                "secretsmanager:DescribeSecret",
+                            ],
+                            resources=[
+                                self.rds_construct.db_instance.secret.secret_arn
+                            ],
+                        ),
+                        iam.PolicyStatement(
+                            actions=["rds-db:connect"],
+                            resources=[
+                                "*"
+                            ],  # Adjust this to specific RDS resources if required
+                        ),
+                    ]
+                )
+            },
+        )
+
+        self.task_definition = ecs.FargateTaskDefinition(
+            self,
+            "sql-task-definition",
+            memory_limit_mib=512,
+            cpu=256,
+            execution_role=iam.Role(
+                self,
+                "sql-task-execution-role",
+                role_name=f"{config.project_name}-sql-task-execution-role",
+                assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+                managed_policies=[
+                    iam.ManagedPolicy.from_aws_managed_policy_name(
+                        "service-role/AmazonECSTaskExecutionRolePolicy"
+                    )
+                ],
+            ),
+            task_role=task_role,
+        )
+
+        self.task_definition.add_container(
+            "sql-container",
+            image=ecs.ContainerImage.from_asset(
+                "infra", file="setup/deploy_sql/Dockerfile"
+            ),
+            command=[
+                "sh",
+                "-c",
+                'for file in /schema/*.sql; do echo "Running $file"; psql postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:5432/throwbackrequestlive -f $file; done',
+            ],
+            logging=ecs.LogDrivers.aws_logs(
+                stream_prefix="sql-deployment",
+                log_group=logs.LogGroup(
+                    self,
+                    "sql-container-log-group",
+                    log_group_name=f"/{config.project_name}-sql-container-logs-{self.node.id}",
+                    removal_policy=RemovalPolicy.DESTROY,
+                ),
+            ),
+            secrets={
+                "DB_USER": ecs.Secret.from_secrets_manager(
+                    self.rds_construct.db_instance.secret, field="username"
+                ),
+                "DB_PASSWORD": ecs.Secret.from_secrets_manager(
+                    self.rds_construct.db_instance.secret, field="password"
+                ),
+            },
+            environment={
+                "DB_HOST": self.rds_construct.db_instance.db_instance_endpoint_address,
+            },
+        )
+
+        self.security_group = ec2.SecurityGroup(
+            self,
+            "TaskSecurityGroup",
+            vpc=vpc,
+            description="Allow ECS tasks to communicate with RDS",
+            allow_all_outbound=True,
+        )
+        self.security_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(), ec2.Port.tcp(5432), "Allow PostgreSQL access"
+        )
+
+        admin_policy = iam.ManagedPolicy(
+            self,
+            "admin-rds-policy",
+            statements=[
+                iam.PolicyStatement(
+                    actions=[
+                        "rds-db:connect",
+                        "rds-db:executeStatement",
+                        "rds-db:batchExecuteStatement",
+                    ],
+                    resources=[self.rds_construct.db_instance.instance_arn],
+                )
+            ],
+        )
+
+        admin_role = iam.Role(
+            self,
+            "admin-role",
+            assumed_by=iam.ServicePrincipal("cognito-idp.amazonaws.com"),
+        )
+
+        admin_role.add_managed_policy(admin_policy)
--- infra/stacks/storage.py
+++ infra/stacks/storage.py
@@ -1,8 +1,5 @@
-from aws_cdk import CfnOutput, RemovalPolicy
+from aws_cdk import CfnOutput
 from aws_cdk import aws_ec2 as ec2
-from aws_cdk import aws_ecs as ecs
-from aws_cdk import aws_iam as iam
-from aws_cdk import aws_logs as logs
 from config import Config
 from constructs import Construct
 from constructs.rds import RdsConstruct
@@ -18,126 +15,16 @@ class StorageStack(Stack):
     ):
         super().__init__(scope, config, suffix="storage")
 
-        self.rds_construct = RdsConstruct(
-            self, "rds", vpc=vpc, project_name=config.project_name
-        )
+        self.rds_construct = RdsConstruct(self, "rds", vpc, config)
 
-        sql_task_role = iam.Role(
+        CfnOutput(
             self,
-            "SQLTaskRole",
-            assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-            inline_policies={
-                "SQLTaskPolicy": iam.PolicyDocument(
-                    statements=[
-                        iam.PolicyStatement(
-                            actions=[
-                                "secretsmanager:GetSecretValue",
-                                "secretsmanager:DescribeSecret",
-                            ],
-                            resources=[
-                                self.rds_construct.db_instance.secret.secret_arn
-                            ],
-                        ),
-                        iam.PolicyStatement(
-                            actions=["rds-db:connect"],
-                            resources=[
-                                "*"
-                            ],  # Adjust this to specific RDS resources if required
-                        ),
-                    ]
-                )
-            },
+            "security-group-id",
+            value=self.rds_construct.security_group.security_group_id,
         )
-
-        sql_task_definition = ecs.FargateTaskDefinition(
-            self,
-            "sql-task-definition",
-            memory_limit_mib=512,
-            cpu=256,
-            execution_role=iam.Role(
-                self,
-                "sql-task-execution-role",
-                role_name=f"{config.project_name}-sql-task-execution-role",
-                assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-                managed_policies=[
-                    iam.ManagedPolicy.from_aws_managed_policy_name(
-                        "service-role/AmazonECSTaskExecutionRolePolicy"
-                    )
-                ],
-            ),
-            task_role=sql_task_role,
-        )
-
-        sql_task_definition.add_container(
-            "sql-container",
-            image=ecs.ContainerImage.from_asset(
-                "infra", file="setup/deploy_sql/Dockerfile"
-            ),
-            command=[
-                "sh",
-                "-c",
-                'for file in /schema/*.sql; do echo "Running $file"; psql postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:5432/throwbackrequestlive -f $file; done',
-            ],
-            logging=ecs.LogDrivers.aws_logs(
-                stream_prefix="sql-deployment",
-                log_group=logs.LogGroup(
-                    self,
-                    "sql-container-log-group",
-                    log_group_name=f"/{config.project_name}-sql-container-logs-{self.node.id}",
-                    removal_policy=RemovalPolicy.DESTROY,
-                ),
-            ),
-            secrets={
-                "DB_USER": ecs.Secret.from_secrets_manager(
-                    self.rds_construct.db_instance.secret, field="username"
-                ),
-                "DB_PASSWORD": ecs.Secret.from_secrets_manager(
-                    self.rds_construct.db_instance.secret, field="password"
-                ),
-            },
-            environment={
-                "DB_HOST": self.rds_construct.db_instance.db_instance_endpoint_address,
-            },
-        )
-
-        security_group = ec2.SecurityGroup(
-            self,
-            "TaskSecurityGroup",
-            vpc=vpc,
-            description="Allow ECS tasks to communicate with RDS",
-            allow_all_outbound=True,
-        )
-        security_group.add_ingress_rule(
-            ec2.Peer.any_ipv4(), ec2.Port.tcp(5432), "Allow PostgreSQL access"
-        )
-
-        CfnOutput(self, "security-group-id", value=security_group.security_group_id)
 
         CfnOutput(
             self,
             "sql-task-definition-arn",
-            value=sql_task_definition.task_definition_arn,
+            value=self.rds_construct.task_definition.task_definition_arn,
         )
-
-        admin_policy = iam.ManagedPolicy(
-            self,
-            "admin-rds-policy",
-            statements=[
-                iam.PolicyStatement(
-                    actions=[
-                        "rds-db:connect",
-                        "rds-db:executeStatement",
-                        "rds-db:batchExecuteStatement",
-                    ],
-                    resources=[self.rds_construct.db_instance.instance_arn],
-                )
-            ],
-        )
-
-        self.admin_role = iam.Role(
-            self,
-            "admin-role",
-            assumed_by=iam.ServicePrincipal("cognito-idp.amazonaws.com"),
-        )
-
-        self.admin_role.add_managed_policy(admin_policy)
