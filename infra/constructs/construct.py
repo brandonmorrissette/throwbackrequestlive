234aa687335d90873a7d1dab657927fddf659ef2
Officer Admin <developeralt@protonmail.com>
2025-02-10 11:25:21 -0500

    Trying positional args only

--- infra/constructs/construct.py
+++ infra/constructs/construct.py
@@ -5,7 +5,7 @@ from config import Config
 from resources.resource import Resource
 
 
-class Construct(AwsCdKConstruct):
+class Construct:
     def __init__(
         self,
         scope: AwsCdKConstruct,
@@ -15,4 +15,6 @@ class Construct(AwsCdKConstruct):
     ) -> None:
         # Resource.__init__(self, scope, config, id, suffix)
         # super().__init__(scope, id, env=config.cdk_environment)
-        logging.info("Construct sanity check validation")
+        logging.info(
+            f"Construct sanity check validation : {scope} {config} {id} {suffix}"
+        )
