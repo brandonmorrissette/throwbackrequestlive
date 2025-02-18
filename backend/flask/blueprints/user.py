bcd2fc4e015789b49d88d0eb077635480faece37
Officer Admin <developeralt@protonmail.com>
2025-02-18 17:54:26 -0500

    Fixing environment config. Updating how read shows gets data

--- backend/flask/blueprints/user.py
+++ backend/flask/blueprints/user.py
@@ -1,4 +1,5 @@
 from blueprints.blueprint import BaseBlueprint
+from decorators.auth import restrict_access
 from flask import current_app as app
 from flask import jsonify, request
 from services.cognito import CognitoService
@@ -10,12 +11,14 @@ class UserBlueprint(BaseBlueprint):
     def _register_routes(self):
 
         @self._blueprint.route("/users", methods=["GET"])
+        @restrict_access(["superuser"])
         def read_rows():
             users = self._service.read_rows()
             app.logger.debug(f"Listing users: {users}")
             return jsonify(users), 200
 
         @self._blueprint.route("/users", methods=["PUT"])
+        @restrict_access(["superuser"])
         def write_rows():
             response = request.json
             app.logger.debug(f"Writing users: {response}")
