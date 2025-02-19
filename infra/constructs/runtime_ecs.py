932a79c56f29e641f24fefc38facfe434606ad8d
Officer Admin <developeralt@protonmail.com>
2025-02-19 17:43:40 -0500

    Another pattern for retrieving secret from jwt

--- infra/constructs/runtime_ecs.py
+++ infra/constructs/runtime_ecs.py
@@ -119,7 +119,9 @@ class RuntimeEcsConstruct(Construct):
                     "DB_HOST": ecs.Secret.from_secrets_manager(
                         db_instance.secret, field="host"
                     ),
-                    "JWT_SECRET": ecs.Secret.from_secrets_manager(jwt_secret.secret),
+                    "JWT_SECRET": ecs.Secret.from_secrets_manager(
+                        jwt_secret.secret_value
+                    ),
                 },
                 task_role=task_role,
             ),
