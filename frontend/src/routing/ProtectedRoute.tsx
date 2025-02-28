e42d562bb864d96d5df79a8fc9e397ca58bf6df9
Officer Admin <developeralt@protonmail.com>
2025-02-28 16:36:45 -0500

    Adding some depth to the Protectedroute

--- frontend/src/routing/ProtectedRoute.tsx
+++ frontend/src/routing/ProtectedRoute.tsx
@@ -1,3 +1,4 @@
+import jwtDecode from 'jwt-decode';
 import React from 'react';
 
 interface ProtectedRouteProps {
@@ -12,6 +13,19 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
     const token = sessionStorage.getItem('auth_token');
 
     if (!token) {
+        window.location.href = redirectTo;
+        return null;
+    }
+
+    try {
+        const decodedToken: any = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+
+        if (decodedToken.exp < currentTime) {
+            window.location.href = redirectTo;
+            return null;
+        }
+    } catch (error) {
         window.location.href = redirectTo;
         return null;
     }
