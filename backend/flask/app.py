931a193c49e61c8be69b1451f665e0240601d0bd
Officer Admin <developeralt@protonmail.com>
2025-03-02 17:26:14 -0500

    Still trying to find right ref for user_pool_client

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -99,4 +99,4 @@ if __name__ == "__main__":
         config = DevelopmentConfig()
 
     app = _create_app(config)
-    app.run(host="0.0.0.0", port=5000, debug=config.DEBUG)
+    app.run(host="0.0.0.0", port=5000, debug=config.DEBUG)  # nosec B104
