bc276892aa4b476f253332f6963c70ae9f30adf1
Officer Admin <developeralt@protonmail.com>
2025-03-03 13:27:09 -0500

    User pool refactor

--- infra/constructs/userpool.py
+++ infra/constructs/userpool.py
@@ -1,9 +1,9 @@
 """
 This module contains the UserPoolConstruct class, which sets up a Cognito user pool
-and app client, and stores their IDs in SSM Parameter Store.
+and client.
 
 Classes:
-    UserPoolConstruct: A construct that sets up a Cognito user pool and app client.
+    UserPoolConstruct: A construct that sets up a Cognito user pool and client.
 
 Usage example:
     user_pool_construct = UserPoolConstruct(scope, config)
@@ -49,70 +49,83 @@ class UserPoolConstruct(Construct):
         self._cognito_client = boto3.client("cognito-idp")
         user_pool_name = f"{config.project_name}-user-pool"
 
-        self.user_pool = self._user_pool(user_pool_name)
-        self.app_client = self._app_client(user_pool_name)
+        self.user_pool = self._get_user_pool(user_pool_name)
+        user_pool_client = self._get_user_pool_client(user_pool_name)
+        self.user_pool_client_id = (
+            user_pool_client.ref
+            if isinstance(user_pool_client, cognito.CfnUserPoolClient)
+            else user_pool_client.user_pool_client_id
+        )
 
-    def _user_pool(self, user_pool_name):
-        user_pool = self._get_user_pool_by_name(user_pool_name)
-        if not user_pool:
-            user_pool = cognito.UserPool(
-                self,
-                f"{user_pool_name}-{self.node.addr}",
-                user_pool_name=user_pool_name,
-                self_sign_up_enabled=False,
-                sign_in_aliases=cognito.SignInAliases(email=True),
-                password_policy=cognito.PasswordPolicy(
-                    min_length=8,
-                    require_lowercase=False,
-                    require_uppercase=False,
-                    require_digits=False,
-                    require_symbols=False,
-                ),
-                account_recovery=cognito.AccountRecovery.EMAIL_ONLY,
-                removal_policy=RemovalPolicy.DESTROY,
-            )
+    def _get_user_pool(self, user_pool_name):
+        """
+        Retrieves the Cognito user pool with the given name.
+        If the user pool does not exist,
+        it creates a new one.
 
-        return user_pool
+        Args:
+            user_pool_name (str): The name of the user pool.
 
-    def _get_user_pool_by_name(self, user_pool_name):
-        user_pools = self._cognito_client.list_user_pools(MaxResults=60)
-        user_pool = None
-        for pool in user_pools.get("UserPools", []):
+        Returns:
+            cognito.UserPool: The Cognito user pool.
+        """
+        for pool in self._cognito_client.list_user_pools(MaxResults=60).get(
+            "UserPools", []
+        ):
             if pool["Name"] == user_pool_name:
-                user_pool = cognito.UserPool.from_user_pool_id(
+                return cognito.UserPool.from_user_pool_id(
                     self, user_pool_name, user_pool_id=pool["Id"]
                 )
 
-        return user_pool
+        return cognito.UserPool(
+            self,
+            f"{user_pool_name}-{self.node.addr}",
+            user_pool_name=user_pool_name,
+            self_sign_up_enabled=False,
+            sign_in_aliases=cognito.SignInAliases(email=True),
+            password_policy=cognito.PasswordPolicy(
+                min_length=8,
+                require_lowercase=False,
+                require_uppercase=False,
+                require_digits=False,
+                require_symbols=False,
+            ),
+            account_recovery=cognito.AccountRecovery.EMAIL_ONLY,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
 
-    def _app_client(self, project_name):
+    def _get_user_pool_client(self, user_pool_name):
+        """
+        Retrieves the Cognito client for the user pool.
+        If the client does not exist,
+        it creates a new one.
+
+        Args:
+            user_pool_name (str): The name of the user pool.
+
+        Returns:
+            cognito.UserPoolClient: The Cognito client.
+        """
         if not Token.is_unresolved(self.user_pool.user_pool_id):
-            client = self._get_app_client_by_name(
-                project_name + "-user-pool-app-client"
+            app_clients = self._cognito_client.list_user_pool_clients(
+                UserPoolId=self.user_pool.user_pool_id, MaxResults=60
             )
-            if client:
-                return client
+            for client in app_clients.get("UserPoolClients", []):
+                if client["ClientName"] == user_pool_name + "-app-client":
+                    return cognito.UserPoolClient.from_user_pool_client_id(
+                        self,
+                        user_pool_name + "-app-client",
+                        user_pool_client_id=client["ClientId"],
+                    )
 
         return cognito.CfnUserPoolClient(
             self,
-            f"{project_name}-user-pool-app-client",
+            f"{user_pool_name}-user-pool-app-client",
             user_pool_id=self.user_pool.user_pool_id,
-            client_name=f"{project_name}-user-pool-app-client",
+            client_name=f"{user_pool_name}-user-pool-app-client",
             explicit_auth_flows=[
                 "ALLOW_ADMIN_USER_PASSWORD_AUTH",
                 "ALLOW_USER_PASSWORD_AUTH",
                 "ALLOW_REFRESH_TOKEN_AUTH",
             ],
         )
-
-    def _get_app_client_by_name(self, app_client_name):
-        app_clients = self._cognito_client.list_user_pool_clients(
-            UserPoolId=self.user_pool.user_pool_id, MaxResults=60
-        )
-        for client in app_clients.get("UserPoolClients", []):
-            if client["ClientName"] == app_client_name:
-                return cognito.UserPoolClient.from_user_pool_client_id(
-                    self, app_client_name, user_pool_client_id=client["ClientId"]
-                )
-
-        return None
